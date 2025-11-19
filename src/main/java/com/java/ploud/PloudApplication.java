10ed3a3188584bc92677fe6a0e2f9bda369acce7
K-KY <kyuyoungk@naver.com>
2025-11-19 15:28:08 +0900

    Initial Commit

---
+++ src/main/java/com/java/ploud/PloudApplication.java
@@ -0,0 +1,12 @@
+package com.java.ploud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PloudApplication.class, args);
+    }
+}
