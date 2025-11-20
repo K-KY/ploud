cfb86b9d2741ab67b55029be5049bd123fed5bff
K-KY <kyuyoungk@naver.com>
2025-11-20 11:00:09 +0900

    feat: 개발용 cors 설정, 일단 다 열어놓음

---
+++ src/main/java/com/java/ploud/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.java.ploud.config;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NotNull CorsRegistry registry) {
+                registry.addMapping("/**") // 모든 엔드포인트
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 필요한 메서드
+                        .allowedHeaders("*")
+                        .allowCredentials(true); // 쿠키/인증정보 포함 허용 시
+            }
+        };
+    }
+
+}
