0c9dff15437fa4251f9913efaed1ac15627b05f7
K-KY <kyuyoungk@naver.com>
2025-11-22 20:33:19 +0900

    feat: Directory 하위의 파일을 조회하는 메서드 추가

--- src/main/java/com/java/ploud/metadb/service/repository/FileRepository.java
+++ src/main/java/com/java/ploud/metadb/service/repository/FileRepository.java
@@ -3,5 +3,9 @@ package com.java.ploud.metadb.service.repository;
 import com.java.ploud.metadb.service.entity.Files;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FileRepository extends JpaRepository<Files, Long> {
+    List<Files> findByParent_DirSeq(Long parent);
+
 }
