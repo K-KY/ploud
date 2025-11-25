c09cbb0ac5665479d73f2d1ea83ce4a85861de0c
K-KY <kyuyoungk@naver.com>
2025-11-26 02:26:42 +0900

    feat: 현재 경로의 파일, 디렉토리를 반환하는 메서드 추가

--- src/main/java/com/java/ploud/storage/service/MinioService.java
+++ src/main/java/com/java/ploud/storage/service/MinioService.java
@@ -1,9 +1,8 @@
 package com.java.ploud.storage.service;
 
-import io.minio.MinioClient;
-import io.minio.ObjectWriteResponse;
-import io.minio.PutObjectArgs;
+import io.minio.*;
 import io.minio.errors.*;
+import io.minio.messages.Item;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -45,6 +44,18 @@ public class MinioService {
         }
     }
 
+    public Iterable<Result<Item>> readCurrentList(String ownerId, String location) {
+        Iterable<Result<Item>> results = minioClient.listObjects(ListObjectsArgs
+                .builder()
+                .bucket(bucket)
+                .delimiter("/")
+                .prefix(makeStorageName(ownerId, location))
+                .recursive(false)
+                .build());
+
+        return results;
+    }
+
     private void checkBucket() throws InvalidKeyException, IOException, NoSuchAlgorithmException {
         try {
             //버킷 있는지 찾기
@@ -59,6 +70,13 @@ public class MinioService {
         }
     }
 
+    /**
+     * @apiNote ownerId/group/multipartFileName 와 같은 "파일 이름"을 만들어 반환
+     * @param ownerId
+     * @param group
+     * @param multipartFile
+     * @return
+     */
     private String makeStorageName(String ownerId, String group, MultipartFile multipartFile) {
         StringJoiner stringJoiner = new StringJoiner("/", "/", "");
         stringJoiner.add(ownerId);
@@ -67,6 +85,21 @@ public class MinioService {
         }
         stringJoiner.add(multipartFile.getOriginalFilename());
 
+        return stringJoiner.toString();
+    }
+
+    /**
+     * @apiNote - ownerId/group/ 와 같은 "경로"를 만들어 반환
+     * @param ownerId
+     * @param group
+     * @return
+     */
+    private String makeStorageName(String ownerId, String group) {
+        StringJoiner stringJoiner = new StringJoiner("/", "", "/");
+        stringJoiner.add(ownerId);
+        if (group != null) {
+            stringJoiner.add(group);
+        }
         return stringJoiner.toString();
     }
 }
