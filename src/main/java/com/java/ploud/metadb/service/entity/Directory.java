0acb6dd2f64be3800b7afe7a903635621f70d2ae
K-KY <kyuyoungk@naver.com>
2025-11-27 19:15:55 +0900

    feat : Directory에 복합 유니크 제약 추가

--- src/main/java/com/java/ploud/metadb/service/entity/Directory.java
+++ src/main/java/com/java/ploud/metadb/service/entity/Directory.java
@@ -4,18 +4,33 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
+@Table(name="directory", uniqueConstraints = {
+        @UniqueConstraint(
+                name = "dir_owner_parent_unique",
+                columnNames = {"owner_id",
+                        "parent_dir_seq",
+                        "dir_name"}
+        )
+})
 public class Directory {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long dirSeq;
+
+    @Column(name = "dir_name")
     private String dirName;
+    @Column(name = "owner_id")
+    private String ownerId;
 
     @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "parent_dir_seq")
     private Directory parent;
 }
