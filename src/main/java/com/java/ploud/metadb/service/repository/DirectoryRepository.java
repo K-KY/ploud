62134a6b35c3eadf68dec9286e744038d20fbc22
K-KY <kyuyoungk@naver.com>
2025-11-27 19:16:24 +0900

    feat : Directory 조회 메서드

---
+++ src/main/java/com/java/ploud/metadb/service/repository/DirectoryRepository.java
@@ -0,0 +1,19 @@
+package com.java.ploud.metadb.service.repository;
+
+import com.java.ploud.metadb.service.entity.Directory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DirectoryRepository extends JpaRepository<Directory, Long> {
+    Optional<Directory> findByDirNameAndParentAndOwnerId(String dirName, Directory parent, String ownerId);
+
+    boolean existsByOwnerIdAndParentAndDirName(String ownerId, Directory parent, String dirName);
+
+    List<Directory> findByOwnerId(String ownerId);
+
+    Optional<Directory> findByDirName(String dirName);
+
+    Optional<Directory> findByDirNameAndParentIsNull(String dirName);
+}
