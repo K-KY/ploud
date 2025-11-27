0758a62239a9b71ba18a9cd5056d98bd4f2cfd23
K-KY <kyuyoungk@naver.com>
2025-11-27 19:21:13 +0900

    feat : file 업로드 시 폴더의 저장 구조도 같이 저장하도록 수정

--- src/main/java/com/java/ploud/metadb/service/FileService.java
+++ src/main/java/com/java/ploud/metadb/service/FileService.java
@@ -1,5 +1,6 @@
 package com.java.ploud.metadb.service;
 
+import com.java.ploud.metadb.service.dto.FileUploadDto;
 import com.java.ploud.metadb.service.entity.Files;
 import com.java.ploud.metadb.service.repository.FileRepository;
 import io.minio.ObjectWriteResponse;
@@ -13,20 +14,35 @@ import java.util.List;
 @Service
 public class FileService {
     private final FileRepository fileRepository;
+    private final DirectoryService directoryService;
 
-    public FileService(FileRepository fileRepository) {
+    public FileService(FileRepository fileRepository, DirectoryService directoryService) {
         this.fileRepository = fileRepository;
+        this.directoryService = directoryService;
     }
 
+
+    /**
+     * @apiNote  - 폴더로 업로드 시 폴더 이름이 원본 이름에 포함되는 현상
+     * @param multipartFile
+     * @param ownerId
+     * @param upload
+     * @return
+     */
     public Files upload(MultipartFile multipartFile, String ownerId, ObjectWriteResponse upload) {
+
+        //이거를 특정 구분자로 나누고 나눠진 문자열로 dir테이블에 저장
+        //소유자에게 이미 해당하는 경로가 있으면 제외
+        String originalFilename = multipartFile.getOriginalFilename();
 
         Files entity = Files.builder()
                 .ownerSeq(ownerId)
-                .title(multipartFile.getOriginalFilename())
+                .title(originalFilename)
                 .storageKey(upload.object())
                 .originalFilename(multipartFile.getOriginalFilename())
                 .size(multipartFile.getSize())
                 .contentType(multipartFile.getContentType())
+                .parent(directoryService.findLastParent(originalFilename, ownerId))
                 .build();
 
         return fileRepository.save(entity);
