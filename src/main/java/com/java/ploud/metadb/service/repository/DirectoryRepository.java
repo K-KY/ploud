7b45d2f0b9bf1463db9c576547953d140ae04c15
K-KY <kyuyoungk@naver.com>
2025-12-04 23:35:47 +0900

    feat : 비관적 락 조회 메서드 추가

--- src/main/java/com/java/ploud/metadb/service/repository/DirectoryRepository.java
+++ src/main/java/com/java/ploud/metadb/service/repository/DirectoryRepository.java
@@ -1,12 +1,28 @@
 package com.java.ploud.metadb.service.repository;
 
 import com.java.ploud.metadb.service.entity.Directory;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface DirectoryRepository extends JpaRepository<Directory, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT d FROM Directory d WHERE d.dirName = :dirName " +
+            "AND (:parentSeq IS NULL AND d.parent IS NULL OR d.parent.dirSeq = :parentSeq) " +
+            "AND d.ownerId = :ownerId")
+    Optional<Directory> findWithLock(
+            @Param("dirName") String dirName,
+            @Param("parentSeq") Long parentSeq,
+            @Param("ownerId") String ownerId
+    );
+
+
     Optional<Directory> findByDirNameAndParentAndOwnerId(String dirName, Directory parent, String ownerId);
     Optional<Directory> findByDirNameAndParentDirSeqAndOwnerId(String dirName, Long parent, String ownerId);
 
