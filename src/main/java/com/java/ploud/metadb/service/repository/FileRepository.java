5eaddf8fb173b9b1dc5b604741751f9f35827bc4
K-KY <kyuyoungk@naver.com>
2025-12-05 23:19:36 +0900

    feat: 사용자 아이디와 부모 디렉토리 번호로 파일 탐색 메서드

--- src/main/java/com/java/ploud/metadb/service/repository/FileRepository.java
+++ src/main/java/com/java/ploud/metadb/service/repository/FileRepository.java
@@ -7,5 +7,7 @@ import java.util.List;
 
 public interface FileRepository extends JpaRepository<Files, Long> {
     List<Files> findByParent_DirSeq(Long parent);
+    // todo : Directory에서는 OwnerId로 쓰고 있음 통일 필요
+    List<Files> findByOwnerSeqAndParent_DirSeq(String ownerId, Long parent);
 
 }
