4676204d7058d919f5595b35e491fbb29fb2d490
K-KY <kyuyoungk@naver.com>
2025-12-09 19:43:35 +0900

    feat: fileDto 추가

---
+++ src/main/java/com/java/ploud/metadb/service/dto/FileDto.java
@@ -0,0 +1,35 @@
+package com.java.ploud.metadb.service.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+public class FileDto {
+    @Getter
+    @Builder
+    public static class Request {
+        private String ownerId;
+        private Long parentSeq;
+
+        public Request(String ownerId, Long parentSeq) {
+            this.ownerId = ownerId;
+            this.parentSeq = parentSeq;
+        }
+    }
+
+    @Getter
+    @Builder
+    public static class Response {
+        private Long dirSeq;
+        private String ownerId;
+        private String fileName;
+        private Long parentSeq;
+
+        public Response(Long dirSeq, String ownerId, String fileName, Long parentSeq) {
+            this.dirSeq = dirSeq;
+            this.ownerId = ownerId;
+            this.fileName = fileName;
+            this.parentSeq = parentSeq;
+        }
+    }
+
+}
