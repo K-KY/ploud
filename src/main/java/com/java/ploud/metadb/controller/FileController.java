064957dd7d14281af794afba75244c221a62461a
K-KY <kyuyoungk@naver.com>
2025-12-18 00:59:49 +0900

    refactor: FileController에서 스토리지 관련 로직 분리 및 요청 파라미터로 MetaDataDto 사용하도록 변경

--- src/main/java/com/java/ploud/metadb/controller/FileController.java
+++ src/main/java/com/java/ploud/metadb/controller/FileController.java
@@ -2,43 +2,41 @@ package com.java.ploud.metadb.controller;
 
 import com.java.ploud.metadb.service.FileService;
 import com.java.ploud.metadb.service.dto.FileDto;
+import com.java.ploud.metadb.service.dto.MetaDataDto;
 import com.java.ploud.metadb.service.entity.Files;
-import com.java.ploud.storage.service.MinioService;
-import io.minio.ObjectWriteResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/files")
 public class FileController {
     private final FileService fileService;
-    private final MinioService minioService;
 
-    public FileController(FileService fileService, MinioService minioService) {
+    public FileController(FileService fileService) {
         this.fileService = fileService;
-        this.minioService = minioService;
     }
 
-
-    @PostMapping("upload")
+    @PostMapping(value = "upload", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> upload(
-            @RequestParam("file") MultipartFile file,
-            @RequestParam(value = "ownerId") String ownerId,
-            @RequestParam(value = "group") String group) {
+            @RequestBody MetaDataDto request
+    ) {
         try {
-            //스토리지
-            ObjectWriteResponse upload = minioService.upload(file, ownerId, group);
+            Files saved = fileService.upload(request);
+            return ResponseEntity.ok(saved);
 
-            //메타데이터
-            Files saved = fileService.upload(file, ownerId, upload);
-
-            return ResponseEntity.status(HttpStatus.OK).body(saved);
         } catch (Exception e) {
-            return ResponseEntity.status(500).body("Upload failed: " + e.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Map.of(
+                            "success", false,
+                            "message", e.getMessage()
+                    ));
         }
     }
 
--- src/main/java/com/java/ploud/metadb/service/FileService.java
+++ src/main/java/com/java/ploud/metadb/service/FileService.java
@@ -1,12 +1,11 @@
 package com.java.ploud.metadb.service;
 
+import com.java.ploud.metadb.service.dto.MetaDataDto;
 import com.java.ploud.metadb.service.entity.Directory;
 import com.java.ploud.metadb.service.entity.Files;
 import com.java.ploud.metadb.service.repository.FileRepository;
-import io.minio.ObjectWriteResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.nio.file.Paths;
 import java.util.List;
@@ -24,26 +23,23 @@ public class FileService {
 
 
     /**
-     * @apiNote  - 폴더로 업로드 시 폴더 이름이 원본 이름에 포함되는 현상
-     * @param multipartFile
-     * @param ownerId
-     * @param upload
+     * @param metaDataDto
      * @return
+     * @apiNote - 폴더로 업로드 시 폴더 이름이 원본 이름에 포함되는 현상
      */
-    public Files upload(MultipartFile multipartFile, String ownerId, ObjectWriteResponse upload) {
-
+    public Files upload(MetaDataDto metaDataDto) {
         //이거를 특정 구분자로 나누고 나눠진 문자열로 dir테이블에 저장
         //소유자에게 이미 해당하는 경로가 있으면 제외
-        String originalFilename = multipartFile.getOriginalFilename();
+        String originalFilename = metaDataDto.getOriginalFilename();
 
         Files entity = Files.builder()
-                .ownerSeq(ownerId)
+                .ownerSeq(metaDataDto.getOwnerId())
                 .title(Paths.get(originalFilename).getFileName().toString())
-                .storageKey(upload.object())
-                .originalFilename(multipartFile.getOriginalFilename())
-                .size(multipartFile.getSize())
-                .contentType(multipartFile.getContentType())
-                .parent(getLastParent(ownerId, originalFilename))
+                .storageKey(metaDataDto.getStorageKey())
+                .originalFilename(originalFilename)
+                .size(metaDataDto.getSize())
+                .contentType(metaDataDto.getContentType())
+                .parent(getLastParent(metaDataDto.getOwnerId(), originalFilename))
                 .build();
 
         return fileRepository.save(entity);
