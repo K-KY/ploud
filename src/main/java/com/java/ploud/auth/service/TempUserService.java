03869474fa12573668a382d8ee07a9cc990a70c2
K-KY <kyuyoungk@naver.com>
2025-12-28 23:16:17 +0900

    feat: 사용자 임시저장, 조회 서비스
    - 수정은 불필요
    - 유효기간이 있기 때문에 삭제는 만들어두지 않음

---
+++ src/main/java/com/java/ploud/auth/service/TempUserService.java
@@ -0,0 +1,40 @@
+package com.java.ploud.auth.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.java.ploud.auth.dto.UserDto;
+import com.java.ploud.auth.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class TempUserService {
+
+    private final StringRedisTemplate stringRedisTemplate;
+    private final ObjectMapper objectMapper;
+
+    //인증되지 않은 사용자 임시저장
+    public String save(UserDto.Request user) throws JsonProcessingException {
+        String token = UUID.randomUUID().toString();
+        String key = "temp_user_" + token;
+
+        String jsonString = objectMapper.writeValueAsString(user);
+        stringRedisTemplate.opsForValue().set(key, jsonString, Duration.ofMinutes(10));
+        return token;
+    }
+
+    //임시저장된 데이터를 영속화 하기 위해 조회
+    public UserDto.Request findByToken(String token) throws JsonProcessingException {
+        String key = "temp_user_" + token;
+        String jsonString = stringRedisTemplate.opsForValue().get(key);
+        if (jsonString == null) {
+            return null;
+        }
+        return objectMapper.readValue(jsonString, UserDto.Request.class);
+    }
+}
