96715c3212834769c37800c85a593f5d5832c0cc
K-KY <kyuyoungk@naver.com>
2025-12-28 23:18:29 +0900

    feat: 발급한 토큰으로 사용자를 영속화 하는 기능 추가

--- src/main/java/com/java/ploud/auth/controller/SignUpController.java
+++ src/main/java/com/java/ploud/auth/controller/SignUpController.java
@@ -1,9 +1,12 @@
 package com.java.ploud.auth.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.java.ploud.auth.dto.UserDto;
 import com.java.ploud.auth.entity.User;
+import com.java.ploud.auth.service.TempUserService;
 import com.java.ploud.auth.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class SignUpController {
 
+    private final TempUserService tempUserService;
     private final UserService userService;
 
     @PostMapping
-    public UserDto.Response signUp(@RequestBody UserDto.Request dto) {
-        User user = userService.createUser(dto);
-        return UserDto.of(user);
+    public void signUp(@RequestBody UserDto.Request dto) throws JsonProcessingException {
+        String token = tempUserService.save(dto);
+        //todo 이메일 전송
     }
 
+    @GetMapping
+    public UserDto.Response verify(String token) throws JsonProcessingException {
+        UserDto.Request tempUser = tempUserService.findByToken(token);
+        User user = userService.createUser(tempUser);//임시저장된 데이터 영속화
+        return UserDto.of(user);
+    }
 }
