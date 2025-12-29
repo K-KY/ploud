cd588686e0246b26168d837cd644324ecab83935
K-KY <kyuyoungk@naver.com>
2025-12-29 23:51:42 +0900

    feat: 사용자 데이터 저장시 비밀번호 암호화

--- src/main/java/com/java/ploud/auth/service/UserService.java
+++ src/main/java/com/java/ploud/auth/service/UserService.java
@@ -4,12 +4,15 @@ import com.java.ploud.auth.dto.UserDto;
 import com.java.ploud.auth.entity.User;
 import com.java.ploud.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
 
     //todo : 이메일 인증
     public User createUser(UserDto.Request dto) {
@@ -19,7 +22,7 @@ public class UserService {
 
         User user = User.builder()
                 .userEmail(dto.getEmail())
-                .password(dto.getPassword())
+                .password(passwordEncoder.encode(dto.getPassword()))
                 .userName(dto.getUserName()).build();
         userRepository.save(user);
         return user;
