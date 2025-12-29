b1c8af24aad225598b63886e4c06c8638c4382a5
K-KY <kyuyoungk@naver.com>
2025-12-30 00:12:35 +0900

    refactor: 임시저장할 때부터 데이터를 암호화

--- src/main/java/com/java/ploud/auth/controller/SignUpController.java
+++ src/main/java/com/java/ploud/auth/controller/SignUpController.java
@@ -3,6 +3,7 @@ package com.java.ploud.auth.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.java.ploud.auth.dto.UserDto;
 import com.java.ploud.auth.entity.User;
+import com.java.ploud.auth.service.PasswordEncryptor;
 import com.java.ploud.auth.service.TempUserService;
 import com.java.ploud.auth.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -17,10 +18,12 @@ public class SignUpController {
 
     private final TempUserService tempUserService;
     private final UserService userService;
+    private final PasswordEncryptor passwordEncryptor;
 
     @PostMapping
     public void signUp(@RequestBody UserDto.Request dto) throws JsonProcessingException {
-        String token = tempUserService.save(dto);
+        UserDto.Request encrypted = passwordEncryptor.encrypt(dto);
+        String token = tempUserService.save(encrypted);
         //todo 이메일 전송
     }
 
