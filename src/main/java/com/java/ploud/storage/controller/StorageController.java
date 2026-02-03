ab592f14ce190bd8d0da9d941e4584f2ee5feafd
K-KY <kyuyoungk@naver.com>
2026-02-03 23:39:54 +0900

    refactor: 컨트롤러 반환타입 수정

--- src/main/java/com/java/ploud/storage/controller/StorageController.java
+++ src/main/java/com/java/ploud/storage/controller/StorageController.java
@@ -2,6 +2,7 @@ package com.java.ploud.storage.controller;
 
 import com.java.ploud.storage.service.dto.FileUploadDto;
 import com.java.ploud.storage.service.MinioService;
+import com.java.ploud.storage.service.dto.PreSignedUrlDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +16,7 @@ public class StorageController {
 
     private final MinioService minioService;
     @PostMapping
-    public ResponseEntity<List<String>> getPreSignedUrl(@RequestBody FileUploadDto.Request request) {
+    public ResponseEntity<List<PreSignedUrlDto.Response>> getPreSignedUrl(@RequestBody FileUploadDto.Request request) {
         return ResponseEntity.ok()
                 .body(minioService.getPreSignedUrl(request.getOwnerId(), request.getFileNames()));
     }
