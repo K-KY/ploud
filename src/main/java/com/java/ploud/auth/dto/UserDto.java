e831d3af63677f1daafe9456fe65fc32552e492b
K-KY <kyuyoungk@naver.com>
2026-02-23 22:25:58 +0900

    feat: 요청 dto에 기본 생성자 추가

--- src/main/java/com/java/ploud/auth/dto/UserDto.java
+++ src/main/java/com/java/ploud/auth/dto/UserDto.java
@@ -4,12 +4,14 @@ import com.java.ploud.auth.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 public class UserDto {
 
     @Getter
     @Builder
     @AllArgsConstructor
+    @NoArgsConstructor
     public static class Request {
         private String userName;
         private String userEmail;
