8275a35ea387bee81e5d0392f3ad5afe6ec0a407
K-KY <kyuyoungk@naver.com>
2026-02-25 23:43:20 +0900

    feat: refreshToken 발급 api

---
+++ src/main/java/com/java/ploud/jwt/controller/JwtController.java
@@ -0,0 +1,32 @@
+package com.java.ploud.jwt.controller;
+
+import com.java.ploud.auth.dto.AuthedUserDetail;
+import com.java.ploud.auth.entity.User;
+import com.java.ploud.auth.service.UserService;
+import com.java.ploud.jwt.dto.JwtDto;
+import com.java.ploud.jwt.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class JwtController {
+    private final JwtService jwtService;
+    private final UserService userService;
+
+    @PostMapping("refresh")
+    public ResponseEntity<JwtDto.Response> refresh(@CookieValue("refresh_token") String token) {
+        String tokenUser = jwtService.findAuthSeqByRefreshTokenId(token);
+        User byUserSeq = userService.findByUserSeq(Long.parseLong(tokenUser));
+        String accessToken = jwtService.createAccessToken(AuthedUserDetail.builder()
+                .userEmail(byUserSeq.getUserEmail())
+                .userSeq(byUserSeq.getUserSeq())
+                .userPassword(byUserSeq.getPassword())
+                .role("ROLE_USER")
+                .build());
+        return ResponseEntity.ok(new JwtDto.Response(accessToken));
+    }
+}
