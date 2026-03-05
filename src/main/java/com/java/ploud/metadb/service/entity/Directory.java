14c4280a145a69165e60b00105a086c6f1cf32ea
K-KY <kyuyoungk@naver.com>
2026-03-05 23:14:36 +0900

    feat: 유니크 제약조건을 User 엔티티 사용하도록 변경

--- src/main/java/com/java/ploud/metadb/service/entity/Directory.java
+++ src/main/java/com/java/ploud/metadb/service/entity/Directory.java
@@ -3,7 +3,11 @@ package com.java.ploud.metadb.service.entity;
 import com.java.ploud.auth.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Builder
 @Getter
@@ -13,7 +17,7 @@ import lombok.*;
 @Table(name = "directory", uniqueConstraints = {
         @UniqueConstraint(
                 name = "dir_owner_parent_unique",
-                columnNames = {"owner_id",
+                columnNames = {"user_seq",
                         "parent_dir_seq",
                         "dir_name"}
         )
@@ -28,6 +32,7 @@ public class Directory {
     private String dirName;
 
     @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_seq")
     private User user;
 
     @ManyToOne(fetch = FetchType.EAGER)
