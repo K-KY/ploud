71ced908afdde22bd39e65f7aa01fac39744fa4f
K-KY <kyuyoungk@naver.com>
2026-03-05 23:17:08 +0900

    feat: 생성 시간 입력을 위해 EntityListeners 추가

--- src/main/java/com/java/ploud/metadb/service/entity/Files.java
+++ src/main/java/com/java/ploud/metadb/service/entity/Files.java
@@ -7,6 +7,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -15,6 +16,7 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Files {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
