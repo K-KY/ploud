7e0b86ad3640a822f42471b760caf57fb761951f
K-KY <kyuyoungk@naver.com>
2026-03-05 23:15:42 +0900

    feat: EnableJpaAuditing 어노테이션 추가
    - 이게 있어야 CreatedDate로 자동으로 들어감

--- src/main/java/com/java/ploud/PloudApplication.java
+++ src/main/java/com/java/ploud/PloudApplication.java
@@ -2,8 +2,10 @@ package com.java.ploud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PloudApplication {
 
     public static void main(String[] args) {
