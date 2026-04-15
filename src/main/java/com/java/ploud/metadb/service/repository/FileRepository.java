c95f4ab21eee29e16f416eb8c76eb59e5361f164
K-KY <kyuyoungk@naver.com>
2026-04-15 22:11:20 +0900

    feat: 사용자 fk와 파일 pk 로 메타데이터 삭제 메서드

--- src/main/java/com/java/ploud/metadb/service/repository/FileRepository.java
+++ src/main/java/com/java/ploud/metadb/service/repository/FileRepository.java
@@ -8,4 +8,6 @@ import java.util.List;
 public interface FileRepository extends JpaRepository<Files, Long> {
     List<Files> findByParent_DirSeq(Long parent);
     List<Files> findByUser_UserSeqAndParent_DirSeq(Long userSeq, Long parent);
+
+    void deleteByUser_userSeqAndFileSeq(Long userUserSeq, Long fileSeq);
 }
