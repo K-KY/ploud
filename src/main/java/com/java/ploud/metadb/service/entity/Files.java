cd59abdf960c62acdb582696f89a57c7b83e962e
K-KY <kyuyoungk@naver.com>
2026-04-18 22:45:26 +0900

    feat: 엔티티 참조 칼럼에 On Delete Cascade 제약조건 추가

--- src/main/java/com/java/ploud/metadb/service/entity/Files.java
+++ src/main/java/com/java/ploud/metadb/service/entity/Files.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -39,6 +41,7 @@ public class Files {
     private String contentType;
 
     @ManyToOne(fetch = FetchType.EAGER)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Directory parent;
 
     private boolean deleted;
