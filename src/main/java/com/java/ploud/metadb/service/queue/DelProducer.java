8a3975b6692039848c6e3c3cbff885e98589db76
K-KY <kyuyoungk@naver.com>
2026-04-26 00:21:02 +0900

    feat: 로그 추가

--- src/main/java/com/java/ploud/metadb/service/queue/DelProducer.java
+++ src/main/java/com/java/ploud/metadb/service/queue/DelProducer.java
@@ -1,6 +1,7 @@
 package com.java.ploud.metadb.service.queue;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -9,10 +10,12 @@ import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class DelProducer {
     private final RedisTemplate<String, Object> redisTemplate;
 
     public void send(Long dirSeq) {
+        log.info("accepted dirSeq={}", dirSeq);
         Map<String, Object> data = new HashMap<>();
         data.put("dirSeq", dirSeq.toString());
 
