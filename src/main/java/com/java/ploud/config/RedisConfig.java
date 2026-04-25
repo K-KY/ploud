3aa94d4d21b64e3680c462b7aa825f47ce666892
K-KY <kyuyoungk@naver.com>
2026-04-25 23:30:59 +0900

    faet: 메세지 큐 용 레디스 빈 등록

--- src/main/java/com/java/ploud/config/RedisConfig.java
+++ src/main/java/com/java/ploud/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.java.ploud.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
@@ -11,5 +12,13 @@ public class RedisConfig {
     @Bean
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
         return new StringRedisTemplate(redisConnectionFactory);
+    }
+
+    //큐로 사용할 레디스 설정
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
     }
 }
