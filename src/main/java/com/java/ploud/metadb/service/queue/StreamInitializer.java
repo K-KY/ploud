d5358269a2982e4a89ea9def6a0725b5846c4896
K-KY <kyuyoungk@naver.com>
2026-04-25 23:55:20 +0900

    faet: 디렉토리 삭제 큐 초기화

---
+++ src/main/java/com/java/ploud/metadb/service/queue/StreamInitializer.java
@@ -0,0 +1,22 @@
+package com.java.ploud.metadb.service.queue;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class StreamInitializer {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    @PostConstruct
+    public void init() {
+        try {
+            redisTemplate.opsForStream().createGroup("del-stream", "del-group");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
