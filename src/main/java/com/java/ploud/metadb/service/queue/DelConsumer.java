6542d5fb71515cf66061c55f999f2d932c737fa4
K-KY <kyuyoungk@naver.com>
2026-04-29 01:03:49 +0900

    feat: 스케줄러 방식으로 변경 및 디렉토리 탐색후 큐 등록 동작 추가
    - 레디스 타임아웃 5초 스케줄러 0.5초 과정에서 레디스 타임아웃마다 예외 발생중임 예외처리를 해서 무시하는 방법도 있지만 리스너로 변경 예정

--- src/main/java/com/java/ploud/metadb/service/queue/DelConsumer.java
+++ src/main/java/com/java/ploud/metadb/service/queue/DelConsumer.java
@@ -1,60 +1,72 @@
 package com.java.ploud.metadb.service.queue;
 
+import com.java.ploud.metadb.service.DeleteDirQueueService;
 import com.java.ploud.metadb.service.DirectoryService;
-import jakarta.annotation.PostConstruct;
+import com.java.ploud.metadb.service.dto.DirDto;
+import com.java.ploud.metadb.service.entity.Directory;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.stream.*;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.util.List;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class DelConsumer {
     private final RedisTemplate<String, Object> redisTemplate;
     private final DirectoryService directoryService;
+    private final DeleteDirQueueService dirQueueService;
 
-    @PostConstruct
-    public void start() {
-        new Thread(this::consume).start();
-    }
 
+    @Scheduled(fixedDelay = 500)
     public void consume() {
-        while (true) {
-            // 스트림 이름, 필드 key, 필드 value
-            List<MapRecord<String, Object, Object>> messages =
-                    redisTemplate.opsForStream().read(
-                            Consumer.from("del-group", "consumer-1"),
-                            StreamReadOptions.empty().count(1).block(Duration.ofSeconds(5)),
-                            StreamOffset.create("del-stream", ReadOffset.lastConsumed())
-                    );
+        log.info("scheduler running");
+        // 스트림 이름, 필드 key, 필드 value
+        List<MapRecord<String, Object, Object>> messages =
+                redisTemplate.opsForStream().read(
+                        Consumer.from("del-group", "consumer-1"),
+                        StreamReadOptions.empty().count(1).block(Duration.ofSeconds(5)),
+                        StreamOffset.create("del-stream", ReadOffset.lastConsumed())
+                );
 
-            if (messages == null) continue;
+            if (messages == null) return;
 
-            for (MapRecord<String, Object, Object> msg : messages) {
-                try {
-                    System.out.println("처리: " + msg.getValue());
+        for (MapRecord<String, Object, Object> msg : messages) {
+            try {
+                log.info("consume msg {}", msg.getValue());
 
-                    //
-                    process(msg);
+                //
+                process(msg);
 
-                    //
-                    redisTemplate.opsForStream()
-                            .acknowledge("del-stream", "del-group", msg.getId());
+                //
+                redisTemplate.opsForStream()
+                        .acknowledge("del-stream", "del-group", msg.getId());
 
-                } catch (Exception e) {
-                    //
-                    System.out.println("처리 실패: " + msg.getId());
-                }
+            } catch (Exception e) {
+                //
+                log.error("consume msg error: {} msg : {}", e, msg.getValue());
             }
         }
     }
 
+
     private void process(MapRecord<String, Object, Object> msg) {
-        System.out.println("msg = " + msg);
-        System.out.println("msg.getValue().get(\"dirSeq\") = " + msg.getValue().get("dirSeq"));
-        System.out.println("msg.getValue().get(\"status\") = " + msg.getValue().get("status"));
+        String userSeq = msg.getValue().get("user").toString();
+        String dirSeq = msg.getValue().get("dir").toString();
+
+        dirQueueService.execute(msg.getValue().get("queueId").toString());
+
+        List<Directory> childDir = directoryService.findChildDir(Long.parseLong(userSeq), Long.parseLong(dirSeq));
+        System.out.println("childDir = " + childDir);
+        log.info("childDir = {}, size = {}", childDir, childDir.size());
+
+        childDir.forEach(child -> {
+            directoryService.deleteDirSoft(Long.parseLong(userSeq), new DirDto.Request(child.getDirSeq()));
+        });
     }
 }
