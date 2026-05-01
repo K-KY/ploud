f2ee2a6791663a238c222ad7e2db1094bf32a7f6
K-KY <kyuyoungk@naver.com>
2026-05-01 22:31:20 +0900

    refactor: 레디스 기본 직렬화 객체 설정

--- src/main/java/com/java/ploud/config/RedisConfig.java
+++ src/main/java/com/java/ploud/config/RedisConfig.java
@@ -5,13 +5,28 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        return new StringRedisTemplate(redisConnectionFactory);
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate(redisConnectionFactory);
+        stringRedisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+
+        stringRedisTemplate.setKeySerializer(stringRedisSerializer);
+        stringRedisTemplate.setHashKeySerializer(stringRedisSerializer);
+
+        stringRedisTemplate.setValueSerializer(stringRedisSerializer);
+        stringRedisTemplate.setHashValueSerializer(stringRedisSerializer);
+
+        stringRedisTemplate.afterPropertiesSet();
+
+        return stringRedisTemplate;
+
     }
 
     //큐로 사용할 레디스 설정
@@ -19,6 +34,12 @@ public class RedisConfig {
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
+        StringRedisSerializer hashValueSerializer = new StringRedisSerializer();
+        template.setKeySerializer(hashValueSerializer);
+        template.setHashKeySerializer(hashValueSerializer);
+        template.setValueSerializer(hashValueSerializer);
+        template.setHashValueSerializer(hashValueSerializer);
+        template.afterPropertiesSet();
         return template;
     }
 }
