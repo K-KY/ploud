aac3bb4aa0bdbdd363d43e995a3e8d72cf83d13a
K-KY <kyuyoungk@naver.com>
2026-05-03 00:49:55 +0900

    feat: 파일 큐 핸들러 임시 구현체

---
+++ src/main/java/com/java/ploud/metadb/service/queue/FileQueueHandler.java
@@ -0,0 +1,27 @@
+package com.java.ploud.metadb.service.queue;
+
+import com.java.ploud.metadb.service.FileService;
+import com.java.ploud.metadb.service.dto.QueueMessage;
+import com.java.ploud.metadb.service.entity.TargetTypes;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class FileQueueHandler implements QueueHandler {
+    private final FileService fileService;
+
+    public FileQueueHandler(FileService fileService) {
+        this.fileService = fileService;
+    }
+
+    @Override
+    public String getType() {
+        return TargetTypes.FILE.getType();
+    }
+
+    @Override
+    public void handle(QueueMessage msg) {
+        log.info("dir queue received: {}", msg.queueId());
+    }
+}
