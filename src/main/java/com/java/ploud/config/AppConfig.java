703cb80116bdd7d9ad6459f41ca096ba84ffde08
K-KY <kyuyoungk@naver.com>
2026-05-03 01:05:25 +0900

    feat: QueueHandleManager 빈 등록
    - DeleteDirQueueService 로 되어있는데 이걸 파일과 공통으로 쓰기 위해 DeleteQueueService로 변경할 지 아니면 파일용 서비스를 따로 만들지 고민중

---
+++ src/main/java/com/java/ploud/config/AppConfig.java
@@ -0,0 +1,20 @@
+package com.java.ploud.config;
+
+import com.java.ploud.metadb.service.DeleteDirQueueService;
+import com.java.ploud.metadb.service.queue.QueueHandleManager;
+import com.java.ploud.metadb.service.queue.QueueHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class AppConfig {
+
+
+    /// 스프링이 알아서  QueueHandler 붙은 구현체 수집해서 주입 해줄거임
+    @Bean
+    public QueueHandleManager queueHandleManager(DeleteDirQueueService deleteDirQueueService,
+                                                 List<QueueHandler> handlers) {
+        return new QueueHandleManager(deleteDirQueueService, handlers);
+    }}
