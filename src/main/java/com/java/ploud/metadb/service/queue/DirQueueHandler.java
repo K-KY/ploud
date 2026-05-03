f98a118105f418b000eaf44a792ea3d2f7eedbac
K-KY <kyuyoungk@naver.com>
2026-05-03 22:10:15 +0900

    feat: 디렉토리 핸들러에서 디렉토리 하위의 파일을 큐에 넣는 동작 추가

--- src/main/java/com/java/ploud/metadb/service/queue/DirQueueHandler.java
+++ src/main/java/com/java/ploud/metadb/service/queue/DirQueueHandler.java
@@ -1,9 +1,11 @@
 package com.java.ploud.metadb.service.queue;
 
 import com.java.ploud.metadb.service.DirectoryService;
+import com.java.ploud.metadb.service.FileService;
 import com.java.ploud.metadb.service.dto.DirDto;
 import com.java.ploud.metadb.service.dto.QueueMessage;
 import com.java.ploud.metadb.service.entity.Directory;
+import com.java.ploud.metadb.service.entity.Files;
 import com.java.ploud.metadb.service.entity.TargetTypes;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -14,9 +16,11 @@ import java.util.List;
 @Component
 public class DirQueueHandler implements QueueHandler {
     private final DirectoryService directoryService;
+    private final FileService fileService;
 
-    public DirQueueHandler(DirectoryService directoryService) {
+    public DirQueueHandler(DirectoryService directoryService, FileService fileService) {
         this.directoryService = directoryService;
+        this.fileService = fileService;
     }
 
     @Override
@@ -32,11 +36,17 @@ public class DirQueueHandler implements QueueHandler {
 
         //하위 항목 조회
         List<Directory> childDir = directoryService.findChildDir(msg.userSeq(), msg.targetSeq());
-        log.info("childDir = {}, size = {}", childDir, childDir.size());
+        log.info("childDirSize = {}", childDir.size());
         childDir.forEach(child -> {
             //하위 디렉토리 항목 큐에 등록
             directoryService.inDeleteQueue(msg.userSeq(), child.getDirSeq());
         });
-        //파일 서비스에서 큐 등록 호출 해야함
+
+        //파일 서비스에서 큐 등록 호출
+        List<Files> childFiles = fileService.readFiles(msg.userSeq(), msg.targetSeq());
+        log.info("childFileSize = {}", childFiles.size());
+        childFiles.forEach(child -> {
+            fileService.inDeleteQueue(msg.userSeq(), child.getFileSeq());
+        });
     }
 }
