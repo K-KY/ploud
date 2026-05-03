1bf9f6da63e698f788844f8255709bf607121fa4
K-KY <kyuyoungk@naver.com>
2026-05-04 02:00:19 +0900

    fix!!!: 큐 무한 증식 버그 수정
    - 핵심 문제점은 deleteDirSoft에서 큐에 메세지를 다시 세이브 하는 부분
    - 요청 -> 메세지 발행 -> 데이터 제거 -> 메세지 수집 -> 하위 데이터 메세지 발행 및 데이터 삭제 -> 데이터 제거에 사용된 메서드에서 다시 메세지 큐 발행 -> 똑같은 메세지 중복

--- src/main/java/com/java/ploud/metadb/service/DeleteQueueService.java
+++ src/main/java/com/java/ploud/metadb/service/DeleteQueueService.java
@@ -20,7 +20,7 @@ public class DeleteQueueService {
 
     @Transactional
     public void save(Long userSeq, Long targetSeq, TargetTypes type) {
-        log.info("queue saved by user {}, dir {}", userSeq, targetSeq);
+        log.info("queue saved by user {}, dir {}, type {}", userSeq, targetSeq, type);
         DeleteQueue delQueue = DeleteQueue.builder()
                 .queueId(type.getPrefix() + UUID.randomUUID())
                 .userSeq(userSeq)
