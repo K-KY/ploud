4e350987033ba430496f9e1826125de79a4ea4b9
K-KY <kyuyoungk@naver.com>
2026-05-15 00:25:22 +0900

    feat: 스토리지 삭제용 엔티티 추가

---
+++ src/main/java/com/java/ploud/metadb/service/entity/StorageClean.java
@@ -0,0 +1,31 @@
+package com.java.ploud.metadb.service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class StorageClean {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cleanSeq;
+    private String storageKey;
+    private boolean executed;
+
+    @CreatedDate
+    private LocalDateTime created;
+    @LastModifiedDate
+    private LocalDateTime modified;
+}
