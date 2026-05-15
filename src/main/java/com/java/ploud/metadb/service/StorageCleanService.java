dc36cf911db5ef4378f2a5595bd9c58f7effb856
K-KY <kyuyoungk@naver.com>
2026-05-15 22:16:59 +0900

    feat: 사용자 PK와 스토리지 큐 PK 로 조회 메서드

--- src/main/java/com/java/ploud/metadb/service/StorageCleanService.java
+++ src/main/java/com/java/ploud/metadb/service/StorageCleanService.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class StorageCleanService {
@@ -22,5 +24,13 @@ public class StorageCleanService {
                 .build());
         storageCleanRepository.save(storageClean);
         applicationEventPublisher.publishEvent(storageKey);
+    }
+
+    public StorageCleanQueue get(Long userSeq, Long targetSeq) {
+        Optional<StorageCleanQueue> storageCleanQueue = storageCleanRepository.findByCleanSeqAndUserSeq(targetSeq, userSeq);
+        if (storageCleanQueue.isPresent()) {
+            return storageCleanQueue.get();
+        }
+        throw new IllegalArgumentException("No storage clean queue found for user seq " + userSeq);
     }
 }
--- src/main/java/com/java/ploud/metadb/service/repository/StorageCleanRepository.java
+++ src/main/java/com/java/ploud/metadb/service/repository/StorageCleanRepository.java
@@ -3,5 +3,8 @@ package com.java.ploud.metadb.service.repository;
 import com.java.ploud.metadb.service.entity.StorageCleanQueue;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StorageCleanRepository extends JpaRepository<StorageCleanQueue, Long> {
+    Optional<StorageCleanQueue> findByCleanSeqAndUserSeq(Long cleanSeq, Long userSeq);
 }
