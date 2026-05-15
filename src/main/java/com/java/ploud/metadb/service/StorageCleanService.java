1d4f5b160705abc369d0aa6b6b506b93d7919322
K-KY <kyuyoungk@naver.com>
2026-05-15 22:46:30 +0900

    feat: 스토리지 삭제 큐 작업 완료 메서드 추가

--- src/main/java/com/java/ploud/metadb/service/StorageCleanService.java
+++ src/main/java/com/java/ploud/metadb/service/StorageCleanService.java
@@ -6,6 +6,7 @@ import com.java.ploud.metadb.service.repository.StorageCleanRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -26,10 +27,13 @@ public class StorageCleanService {
         applicationEventPublisher.publishEvent(storageKey);
     }
 
+    @Transactional
     public StorageCleanQueue get(Long userSeq, Long targetSeq) {
         Optional<StorageCleanQueue> storageCleanQueue = storageCleanRepository.findByCleanSeqAndUserSeq(targetSeq, userSeq);
         if (storageCleanQueue.isPresent()) {
-            return storageCleanQueue.get();
+            StorageCleanQueue storageData = storageCleanQueue.get();
+            storageData.execute();
+            return storageData;
         }
         throw new IllegalArgumentException("No storage clean queue found for user seq " + userSeq);
     }
