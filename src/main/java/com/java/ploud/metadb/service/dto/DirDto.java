b919e33de0c747d30ccf8ed030e866e67210994c
K-KY <kyuyoungk@naver.com>
2026-05-18 23:04:20 +0900

    feat: 디렉토리의 디렉토리 변경 요청 dto

--- src/main/java/com/java/ploud/metadb/service/dto/DirDto.java
+++ src/main/java/com/java/ploud/metadb/service/dto/DirDto.java
@@ -20,6 +20,15 @@ public class DirDto {
 
     @Getter
     @Builder
+    public static class moveDirRequest {
+        private Long dirSeq;//이동할 디렉토리 번호
+        private String dirName;//이동할 디렉토리 이름
+        private Long parentSeq;//현재 디렉토리 번호
+        private Long targetSeq;//이동 목적지 디렉토리 번호
+    }
+
+    @Getter
+    @Builder
     public static class Response {
         private Long dirSeq;
         private String dirName;
