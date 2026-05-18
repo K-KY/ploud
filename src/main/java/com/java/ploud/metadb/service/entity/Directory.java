3d41b3f0e52c1c7604f9de5126c7cde4cc2ab4aa
K-KY <kyuyoungk@naver.com>
2026-05-19 00:19:16 +0900

    feat: Directory 엔티티에 디렉토리 변경 가능 검증 메서드 추가

--- src/main/java/com/java/ploud/metadb/service/entity/Directory.java
+++ src/main/java/com/java/ploud/metadb/service/entity/Directory.java
@@ -59,6 +59,29 @@ public class Directory {
     }
 
     public void changeDir(Directory targetDirectory) {
-        parent = targetDirectory.getParent();
+        validateMovable(targetDirectory);
+        this.parent = targetDirectory;
+    }
+
+    private void validateMovable(Directory targetDirectory) {
+        Directory node = targetDirectory;
+        if (this.getParent() == null) {
+            throw new IllegalArgumentException("루트 디렉토리 이동 불가");
+        }
+
+        while (node != null) {
+            //목적지의 부모 디렉토리의 번호가 이동할 디렉토리 번호와 같으면 예외
+            if (node.getDirSeq().equals(this.getDirSeq())) {
+                throw new IllegalArgumentException("can not move the parent dir to child dir");//todo 커스텀 예외처리
+            }
+
+            Directory nodeParent = node.getParent();
+
+            if (nodeParent != null &&
+                    nodeParent.getDirSeq().equals(this.getParent().getDirSeq())) {
+                break;
+            }
+            node = node.getParent();//한계층 위로 이동하여 검사 지속
+        }
     }
 }
