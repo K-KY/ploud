aa7d437c183c0c7daeb5bd5d7a7e1e36c21dc388
K-KY <kyuyoungk@naver.com>
2026-05-24 00:33:40 +0900

    feat: 순환 참조 검증및 path 검증 분리

--- src/main/java/com/java/ploud/metadb/service/entity/Directory.java
+++ src/main/java/com/java/ploud/metadb/service/entity/Directory.java
@@ -9,7 +9,6 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 @EntityListeners(AuditingEntityListener.class)
 @Entity
@@ -61,23 +60,30 @@ public class Directory {
     }
 
     public void changeDir(Directory target, long[] path) {
-        validateMovable(path);
-        this.parent = target;
-    }
-
-    private void validateMovable(long[] path) {
-
         //root는 parent가 null임
         if (this.getParent() == null) {
             throw new IllegalArgumentException("루트 디렉토리 이동 불가");
         }
+        validateCycle(path);
+        validatePath(target, path);
+        this.parent = target;
+    }
 
+    //path에 meveDir의 key가 포함 되어있지 않은지 확인
+    private void validateCycle(long[] path) {
         //path에 자신의 번호가 포함되는 요청 예외
         //마지막 번호는 자기 자신이므로 생략
         for (int i = 0; i < path.length - 1; i++) {
-            if (Objects.equals(path[i], this.dirSeq)) {
+            if (path[i] == this.dirSeq) {
                 throw new IllegalArgumentException("can not move the parent dir to child dir");//todo 커스텀 예외처리
             }
+        }
+    }
+
+    //path의 마지막 경로(사용자가 현재 보고있는 화면) 와 목적지가 일치하는지 확인
+    private void validatePath(Directory target, long[] path) {
+        if (target.getDirSeq() != path[path.length - 1]) {
+            throw new IllegalArgumentException("target Path not match");
         }
     }
 }
