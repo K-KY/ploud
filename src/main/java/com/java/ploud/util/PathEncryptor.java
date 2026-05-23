69054f9d7745847977d3120cea029fcd4994c845
K-KY <kyuyoungk@naver.com>
2026-05-24 00:31:52 +0900

    feat: Encryptor에 비즈니스 규칙을 포함한 PathEncryptor 추가

---
+++ src/main/java/com/java/ploud/util/PathEncryptor.java
@@ -0,0 +1,30 @@
+package com.java.ploud.util;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class PathEncryptor {
+    private final Encryptor encryptor;
+
+    public PathEncryptor(Encryptor encryptor) {
+        this.encryptor = encryptor;
+    }
+
+    public String encrypt(String path) {
+        try {
+            return encryptor.encrypt(path);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public long[] decrypt(String path) {
+        try {
+            return Arrays.stream(encryptor.decrypt(path).split("/")).mapToLong(Long::parseLong).toArray();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
