f4f27301ceaf7e697648d98afad92d43d290e3b3
K-KY <kyuyoungk@naver.com>
2026-05-24 23:13:31 +0900

    feat: 핸들러가 받을 메세지 dto에 Path 추가

--- src/main/java/com/java/ploud/metadb/service/dto/QueueMessage.java
+++ src/main/java/com/java/ploud/metadb/service/dto/QueueMessage.java
@@ -1,4 +1,4 @@
 package com.java.ploud.metadb.service.dto;
 
-public record QueueMessage(String queueId, String type, Long userSeq, Long targetSeq) {
+public record QueueMessage(String queueId, String type, Long userSeq, Long targetSeq, String path) {
 }
