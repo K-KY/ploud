76d55ff52d382a811e07cb222d8abb11f9f327c9
K-KY <kyuyoungk@naver.com>
2026-05-24 23:47:34 +0900

    ROLLBACK: 큐 관련 메서드에서 path 사용 롤백

--- src/main/java/com/java/ploud/metadb/service/queue/DirQueueHandler.java
+++ src/main/java/com/java/ploud/metadb/service/queue/DirQueueHandler.java
@@ -32,11 +32,10 @@ public class DirQueueHandler implements QueueHandler {
     public void handle(QueueMessage msg) {
         log.info("dir queue received: {}", msg.queueId());
         //발행된 메세지의 디렉토리 삭제 수행
-        directoryService.deleteDirSoft(msg.userSeq(), new DirDto.Request(msg.targetSeq(), msg.path()));
+        directoryService.deleteDirSoft(msg.userSeq(), msg.targetSeq());
 
         //하위 항목 조회
-        List<Directory> childDir = directoryService.findChildDir(msg.userSeq(),
-                new DirDto.Request(msg.targetSeq(), msg.path()));
+        List<Directory> childDir = directoryService.findChildDir(msg.userSeq(), msg.targetSeq());
         log.info("childDirSize = {}", childDir.size());
         childDir.forEach(child -> {
             //하위 디렉토리 항목 큐에 등록
