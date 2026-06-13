ee8d3bd4c5e9748acffbb049e07ad12b1ef8b8b6
K-KY <kyuyoungk@naver.com>
2026-06-14 01:02:26 +0900

    feat: moveDirRequest 생성자 추가, 이름 필드와 현재 디렉토리 필드 제거

--- src/main/java/com/java/ploud/metadb/service/dto/DirDto.java
+++ src/main/java/com/java/ploud/metadb/service/dto/DirDto.java
@@ -3,8 +3,6 @@ package com.java.ploud.metadb.service.dto;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.util.List;
-
 public class DirDto {
 
     @Getter
@@ -26,9 +24,10 @@ public class DirDto {
     @Builder
     public static class moveDirRequest {
         private Long dirSeq;//이동할 디렉토리 번호
-        private String dirName;//이동할 디렉토리 이름
-        private Long parentSeq;//현재 디렉토리 번호
         private Long targetSeq;//이동 목적지 디렉토리 번호
+
+        public moveDirRequest() {
+        }
     }
 
     @Getter
