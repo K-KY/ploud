05ae44b05133507531389bcea6ac07fddaceb89c
K-KY <kyuyoungk@naver.com>
2026-07-04 01:40:18 +0900

    feat: 루트 디렉토리 없는경우 커스텀 예외

---
+++ src/main/java/com/java/ploud/exceptions/RootNotFoundException.java
@@ -0,0 +1,7 @@
+package com.java.ploud.exceptions;
+
+public class RootNotFoundException extends RuntimeException {
+    public RootNotFoundException(String message) {
+        super(message);
+    }
+}
