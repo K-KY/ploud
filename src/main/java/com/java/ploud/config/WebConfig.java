a19a41d322ab723283f2b1ce6e008c1c30ecc96c
K-KY <kyuyoungk@naver.com>
2026-07-10 23:20:33 +0900

    feat: 원격 서버 도메인 cors 설정 추가

--- src/main/java/com/java/ploud/config/WebConfig.java
+++ src/main/java/com/java/ploud/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(@NotNull CorsRegistry registry) {
                 registry.addMapping("/**") // 모든 엔드포인트
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173", "https://kky.tail0a6d17.ts.net")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 필요한 메서드
                         .allowedHeaders("*")
                         .allowCredentials(true); // 쿠키/인증정보 포함 허용 시
