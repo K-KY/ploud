a23965cae2971696ec2f42f35b8296640568bcab
K-KY <kyuyoungk@naver.com>
2026-07-14 23:05:59 +0900

    feat: 외부용 클라이언트와 내부용 클라이언트 빈 생성

--- src/main/java/com/java/ploud/config/MinioConfig.java
+++ src/main/java/com/java/ploud/config/MinioConfig.java
@@ -10,15 +10,26 @@ import org.springframework.context.annotation.Configuration;
 public class MinioConfig {
     @Value("${minio.endpoint}")
     private String endpoint;
+    @Value("${minio.endpoint.internal}")
+    private String internalEndpoint;
     @Value("${minio.access-key}")
     private String accessKey;
     @Value("${minio.secret-key}")
     private String secretKey;
 
-    @Bean
+    @Bean("externalMinioClient")
     public MinioClient minioClient() {
         return MinioClient.builder()
                 .endpoint(endpoint)
+                .region("us-east-1")//**이게 없으면 외부에서 조회 불가능한 경로를 접속해서 알아내려 시도 -> 접근 불가해서 에러
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+
+    @Bean("internalMinioClient")
+    public MinioClient internalMinioClient() {
+        return MinioClient.builder()
+                .endpoint(internalEndpoint)
                 .credentials(accessKey, secretKey)
                 .build();
     }
